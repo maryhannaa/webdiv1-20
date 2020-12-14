f1a2ee9bacce365879469fa129648320068e137f
mary <mhanna@cloum.edu>
2020-12-14 00:28:44 -0600

    js

---
+++ Portfolio-website/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#banner-name").addClass("animated zoomIn");
+    $("#about_img").hover(function(){
+      $(this).addClass("animated pulse");
+      }, function(){
+      $(this).removeClass("animated pulse");
+    });
+    
+    $(function(){
+      $("#navbar a").click(function(e){
+        e.preventDefault();
+        $('html,body').scrollTo(this.hash,this.hash);
+      });
+    });
+  });
